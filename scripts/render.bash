bcc78fb8924de18d51e55906ed9073ba11fac0ae
PatriotRossii <patriotrossii2019@mail.ru>
2023-02-03 14:00:22 +0300

    Remove unused bash script

--- scripts/render.bash
+++
@@ -1,9 +0,0 @@
-for item in `find ~+ -wholename "*/sources/*"`
-do
-	INPUT_PATH="${item}"
-	OUTPUT_DIRECTORY="${item/sources/renders}"
-	OUTPUT_DIRECTORY="${OUTPUT_DIRECTORY%/*}"
-	cd "${OUTPUT_DIRECTORY}"
-	pdflatex -synctex=1 -interaction=nonstopmode --shell-escape --output-directory="${OUTPUT_DIRECTORY}" "${INPUT_PATH}"
-	pdflatex -synctex=1 -interaction=nonstopmode --shell-escape --output-directory="${OUTPUT_DIRECTORY}" "${INPUT_PATH}"
-done
